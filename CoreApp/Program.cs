d7063eadddff1cdbe80197455cb814933c36d245
jagadeep <developerjagadeep@outlook.com>
2023-02-11 16:27:22 -0600

    Updated swagger

--- CoreApp/Program.cs
+++ CoreApp/Program.cs
@@ -21,12 +21,12 @@ if (app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    });
 }
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
